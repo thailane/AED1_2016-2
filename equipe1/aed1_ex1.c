0d60b1b37afe96b34a6f2d10f7383cacac1bb1d7
leticiaamota <leticiaamota@gmail.com>
2016-08-11 19:14:22 -0400

    exercicio 1

---
+++ equipe1/aed1_ex1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	float **matrx;
+	int i,j;
+	matrx=NULL;
+	matrx = malloc (50*sizeof (int *));
+	for (i=0;i<50;i++)
+		matrx[i] = malloc (50*sizeof(int));
+
+	for (i=0;i<50;i++)
+		for(j=0;j<50;j++)
+			matrx[i][j]=0.0;
+
+	for (i=0;i<50;i++)
+	{
+		for(j=0;j<50;j++)
+		{
+			printf ("%.2f ",matrx[i][j]=0.0);
+		}
+	printf("\n");
+	}
+	free(matrx);
+	return 0;
+}
