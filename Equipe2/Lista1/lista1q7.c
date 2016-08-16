1b07cd9f1e2433a6fb7a615a06548c4970038b0e
Botinelly <lucas_botinelly@hotmail.com>
2016-08-16 14:31:12 -0400

    lista1q7

---
+++ Equipe2/Lista1/lista1q7.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main(){
+    int i, l, c, j, acm;
+
+    printf("linhas: \n");
+    scanf("%d", &l);
+
+    printf("colunas: \n");
+    scanf("%d", &c);
+    
+    int matriz[l][c], vetor[c], valor, resultado[l][1];
+    
+
+    printf("Valores da matriz: \n");
+    for(i=0; i<l;i++){
+        for(j=0; j<c;j++){
+            scanf("%d", &valor);
+            matriz[i][j] = valor;
+            valor = 0;        
+        }
+    }
+
+    printf("Valores do vetor: \n");
+    for(j=0; j<c;j++){
+        scanf("%d", &valor);
+        vetor[j] = valor;
+        valor = 0;    
+    }
+    
+    for(i=0; i<l;i++){
+        for(j=0;j<c; j++){
+            acm = acm+(vetor[j] * matriz[i][j]);
+        }            
+        resultado[i][1] = acm;    
+        acm = 0;
+    }
+    printf("Resultado: \n");
+    for(j=0; j<l;j++){
+        printf("%d\n", resultado[j][1]);        
+    }        
+
+	return 0;
+}
