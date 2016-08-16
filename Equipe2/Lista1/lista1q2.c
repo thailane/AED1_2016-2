cc51b719c3162e662d185b9c89ab5fbb1fefe753
Hermann Hernani <hermannhernani@outlook.com>
2016-08-16 12:44:37 -0300

    lista1 questao 2

---
+++ Equipe2/Lista1/lista1q2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	int *v
+	int num;
+	int i, k, aux;
+
+	v = (int*)malloc(5 * sizeof(int));
+
+	for (i=0;i<5;i++)
+	{
+		printf("Digite um valor para V[%d]: ",i);
+		scanf("%d",v+i);
+
+	}
+	
+}
