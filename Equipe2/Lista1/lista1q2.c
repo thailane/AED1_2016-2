4adbe38276a161e89bc752320415891a04584923
Hermann Hernani <hermannhernani@outlook.com>
2016-08-16 13:09:18 -0300

    Alteraçoes na questao 2 da lista 1

--- Equipe2/Lista1/lista1q2.c
+++ Equipe2/Lista1/lista1q2.c
@@ -14,5 +14,21 @@ int main(void){
 		scanf("%d",v+i);
 
 	}
-	
+	for (i=0;i<5;i++)
+	{
+		for(k=i+1;k<5;k++)
+		{
+			if(v[i] > v[k])
+			{
+				aux = v[i];
+				v[i] = v[k];
+				v[k] = aux;
+			}
+		}
+	}
+	for (i=0;i<5;i++)
+	{
+		printf("\nValor de V[%d] = %d",i, v[i]);
+	}
+	return (1);
 }
