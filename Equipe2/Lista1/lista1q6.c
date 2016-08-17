e28e184b34519183123d0538dbe4d1131d2be04f
Botinelly <lucas_botinelly@hotmail.com>
2016-08-17 13:38:58 -0400

    Update lista1q6.c

--- Equipe2/Lista1/lista1q6.c
+++ Equipe2/Lista1/lista1q6.c
@@ -6,30 +6,66 @@ Stephanny Barreto
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() 
-{
-	int n, x=1, espaco, i, j;
-	
-	printf("Digite o numero de linhas: ");
-	scanf("%d", &n);
-	
-	for(i=0; i<n; i++)
-	{
-		for(espaco=1; espaco <= n-i; espaco++)
-			printf("  ");
-		
-		for(j=0; j<=i; j++)
-		{
-			if(j==0 || i==0)
-				x=1;
-			else;
-				x=x*(i-j+1)/j;
-				
-				printf("%4d", x);
-		}
-		printf("\n");
-	
-	}	
-	return 0;
+
+void preencher(int n, int **T) {
+
+  int i,j;
+
+  T[0][0] = 1;
+  for (i = 1; i < n; i++) {
+    T[i][0] = 1;
+    T[i][i] = 1;
+    for (j = 1; j < i; j++) {
+      T[i][j] = T[i-1][j] + T[i-1][j-1];
+    }
+  }
+
+  return;
+}
+
+void imprimir(int n, int **T) {
+  int i,j;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j <= i; j++) {
+      printf("%5d ", T[i][j]);
+    }
+    printf("\n");
+  }
+
+  return;
+}
+
+
+int main() {
+  int n,i,j,**T;
+
+  printf("Quantos numeros do coiso de pascal? ");
+  scanf("%d", &n);
+
+  T = (int **) malloc(n*sizeof(int *));
+  if (T == NULL) {
+    printf("Erow\n");
+    return(-1);
+  }
+  
+  for (i = 0; i < n; i++) {
+    T[i] = (int *) malloc((i+1)*sizeof(int));
+    
+    if (T[i] == NULL) {
+      printf("Erow\n");
+      return(-1);
+    }
+  }
+
+  if (n <= 0) {
+    return(1);
+  }
+
+  preencher(n,T);
+  imprimir(n,T);
+
+  return(0);
 }
