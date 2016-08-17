f7e1fb51bd18e0e9ae0b257ca61ce7728cc2695c
stephbarreto <stephbarreto@me.com>
2016-08-17 15:37:46 -0400

    Add files via upload

---
+++ Equipe2/Lista1/lista1q5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAX 100
+
+int main() 
+{ 
+	int n, i, j, conta[MAX], comp = 0; 
+	float seq[MAX];
+	
+	printf("Digite o tamanho da sequencia");
+	scanf("%d", &n);
+	
+	printf("Sequencia de %d numeros reais", n);	
+	for (i=0, i < n, i++) {
+		scanf("%f", &seq[comp]);
+		for (j=0, seq[j] != seq[comp], j++); 
+		
+		if(j==comp){
+			conta[comp]=1;
+			comp++;
+		}
+		else {
+			conta[j]++; 
+		}
+		
+	}
+	for(j=0, j < comp; j++)
+		printf("O numero %f aparece %d vezes na sequencia.\n", seq[j], conta[j]);
+		return 0;
+}
