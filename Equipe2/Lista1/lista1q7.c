53bbd190d0ed57d06bdd42df47a6f02ff879585f
Stephanny Barreto <stephbarreto@me.com>
2016-08-17 21:32:14 -0400

    Update lista1q7.c

--- Equipe2/Lista1/lista1q7.c
+++ Equipe2/Lista1/lista1q7.c
@@ -8,7 +8,6 @@ Stephanny Barreto
 7. Dada uma matriz real A com m linhas e n colunas e um vetor real V com n elementos, determinar o produto de A por V. 
 
 */
-//7. Dada uma matriz real A com m linhas e n colunas e um vetor real V com n elementos, determinar o produto de A por V. 
 #include<stdio.h>
 
 int main()
