43fda11b8d46782ec56cffb60138ab9943343462
jimorris <pedroaasena@gmail.com>
2016-08-17 20:43:43 -0500

    Update Exe01.c
    padronização do exercício

--- equipe7/Lista_01/Exe01.c
+++ equipe7/Lista_01/Exe01.c
@@ -4,6 +4,16 @@
     Paulo Silveira
     Pedro Sena
     
+1. Reescreva o programa abaixo usando ponteiros
+ 
+    int main ()
+    {
+    float matrx [50][50]; int i,j;
+    for (i=0;i<50;i++)
+    for (j=0;j<50;j++) matrx[i][j]=0.0;
+    return(0); 
+    }
+    
 */    
 
 #include<stdio.h>
