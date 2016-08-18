8be4db445b00c128c873356ee3b3e6d941f755ec
Matheus <espcs50@hotmail.com>
2016-08-17 22:38:18 -0400

    Atualizando ex 1 e 2

--- Equipe4/lista01/002.c
+++ Equipe4/lista01/002.c
@@ -1,56 +1,62 @@
+/*Equipe 4:
+
+Matheus Obando
+Fabíola Maciel
+Jefferson Avilar
+
+2. Faça um programa usando ponteiros, para ordenar 5 números e mostrá­-los ordenados na tela.*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
 
-int main(){
+int main()
+{
 
-    int *vetor;
-    int i,j;
-    int aux;
+    	int *vetor;
+    	int i,j;
+    	int aux;
 
-    vetor=(int*)calloc(5,sizeof(int));
+    	vetor=(int*)calloc(5,sizeof(int));
 
-    for (i=0;i<5;i++){
-
-
-        printf("Digite o elemento %d: ",i+1);
-        scanf("%d",&vetor[i]);
-        printf("\n");
-    }
-
-    for (i=0;i<5;i++){
-
-        printf("%d",vetor[i]);
-
-    }
-
-    printf("\n");
-
-    for (i=4;i>0;i--){
-
-        for (j=0;j<i;j++){
-
-            if(vetor[j]>vetor[j+1]){
-
-                aux=vetor[j];
-                vetor[j]=vetor[j+1];
-                vetor[j+1]=aux;
+    	for (i=0;i<5;i++){
 
 
-            }
+        	printf("Digite o elemento %d: ",i+1);
+        	scanf("%d",&vetor[i]);
+        	printf("\n");
+    	}
 
-        }
-    }
+    	for (i=0;i<5;i++){
+
+        	printf("%d",vetor[i]);
+
+    	}
+
+    	printf("\n");
+
+    	for (i=4;i>0;i--){
+
+        	for (j=0;j<i;j++){
+
+            		if(vetor[j]>vetor[j+1]){
+
+                		aux=vetor[j];
+                		vetor[j]=vetor[j+1];
+                		vetor[j+1]=aux;
+			}
+		}
+    	}
 
 
-    for (i=0;i<5;i++){
+    	for (i=0;i<5;i++){
 
-        printf("%d",vetor[i]);
+        	printf("%d",vetor[i]);
 
-    }
+    	}
 
 
-    return (0);
+    	return (0);
 
 
 }
