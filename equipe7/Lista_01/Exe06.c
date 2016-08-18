6be818caaf5f754e211e4a9a37e33419173e360c
jimorris <pedroaasena@gmail.com>
2016-08-18 20:00:27 -0300

    Update Exe06.c
    ajustes

--- equipe7/Lista_01/Exe06.c
+++ equipe7/Lista_01/Exe06.c
@@ -1,4 +1,12 @@
-//6. Imprimir as n primeiras linhas do triângulo de Pascal
+/* equipe:
+    Fabiana Bezerra
+    Paulo Roberto
+    Paulo Silveira
+    Pedro Sena
+    
+   6. Imprimir as n primeiras linhas do triângulo de Pascal
+   
+*/
 
 #include<stdio.h>
 
@@ -12,21 +20,16 @@
     scanf("%d", &num_linhas);
 
 // logica de preenchimento do triangulo    
-    for(linha=0; linha<num_linhas; linha++)
-    {
-        for(espacos=1; espacos<num_linhas - linha; espacos++)
-        {
+    for(linha=0; linha<num_linhas; linha++){
+        for(espacos=1; espacos<num_linhas - linha; espacos++){
             printf(" ");
         }
-        for(r = 0; r <= linha; r++)
-        {
-            if(linha == 0 || r == 0)
-            {
+        for(r = 0; r <= linha; r++){
+            if(r == 0 || r == linha){
                 elemento = 1;
                 printf("%d ", elemento);
             }
-            else
-            {
+            else{
                 elemento = elemento * (linha - r + 1)/r;
                 printf("%d ", elemento);
             }
