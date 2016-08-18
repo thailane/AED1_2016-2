327b74ef036ab188754ba0072f984cc5708d9134
aluno <you@example.com>
2016-08-17 21:10:47 -0400

    padronizando

--- equipe1/Lista 1/aed1_ex3.c
+++ equipe1/Lista 1/aed1_ex3.c
@@ -1,28 +1,34 @@
-/* equipe:
-	rodrigo oliveira
-	fernando calderaro
-	thailana neves
-	diogo abreu
-	leticia mota		*/
+/* equipe 01:
+	Diogo Abreu
+	Fernando Calderaro
+	Leticia Mota
+	Rodrigo Oliveira
+	Thailana Neves	
 
-//3. Dada uma seqüência de n números, imprimi-la na ordem inversa à da leitura
+	exercicio 03
+
+	Dada uma seqüência de n números, imprimi-la na ordem inversa à da leitura
+*/
 
 #include <stdio.h>
 
-int main () {
-    int n, i, vet[100];
+int main ()
+{
+	int n, i, vet[100];
 
-    printf("Digite o comprimento da sequência: ");
-    scanf("%d", &n);
+	printf("Digite o comprimento da sequência: ");
+	scanf("%d", &n);
 
-    printf("Digite os %d valores da sequência: \n", n);
+	printf("Digite os %d valores da sequência: \n", n);
 
-    for (i=0; i<n; i++){
-        scanf("%d", &vet[i]);
-    }
+	for (i=0; i<n; i++)
+	{
+		scanf("%d", &vet[i]);
+	}
 
-    for (i=n-1; i>=0; i--){
-        printf("%d ", vet[i]);
-    }
-    return 0;
+	for (i=n-1; i>=0; i--)
+	{
+		printf("%d ", vet[i]);
+	}
+	return 0;
 }
