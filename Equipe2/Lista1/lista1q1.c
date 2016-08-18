c1b9f46fb265f9ebeb834f2e3fe84b70d800ba9d
Botinelly <lucas_botinelly@hotmail.com>
2016-08-17 21:31:40 -0400

    Update lista1q1.c

--- Equipe2/Lista1/lista1q1.c
+++ Equipe2/Lista1/lista1q1.c
@@ -4,6 +4,17 @@ Hermann Hernani
 Lucas Botinelly
 Stephanny Barreto
 */
+/*
+Reescreva o programa abaixo usando ponteiros
+
+int main ()
+{
+float matrx [50][50]; int i,j;
+for (i=0;i<50;i++)
+for (j=0;j<50;j++) matrx[i][j]=0.0;
+return(0);
+}
+*/
 #include <stdio.h>
 #include <stdlib.h> 
 int main ()
