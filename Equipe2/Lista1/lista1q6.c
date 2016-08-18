8e47123c78abe028232001c59fea91ad02c4a4b5
Botinelly <lucas_botinelly@hotmail.com>
2016-08-17 20:43:08 -0400

    Update lista1q6.c

--- Equipe2/Lista1/lista1q6.c
+++ Equipe2/Lista1/lista1q6.c
@@ -8,50 +8,56 @@ Stephanny Barreto
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void preencher(int n, int **T) {
+//Preenche a matriz com os valores de triângulo de Pascal
+void preencher(int n, int **T)
+{
 
   int i,j;
 
   T[0][0] = 1;
-  for (i = 1; i < n; i++) {
+  for (i = 1; i < n; i++) 
+  {
     T[i][0] = 1;
     T[i][i] = 1;
-    for (j = 1; j < i; j++) {
+    for (j = 1; j < i; j++) 
+    {
       T[i][j] = T[i-1][j] + T[i-1][j-1];
     }
   }
-
-  return;
 }
 
-void imprimir(int n, int **T) {
+//Imprime a Matriz
+void imprimir(int n, int **T) 
+{
   int i,j;
 
-  for (i = 0; i < n; i++) {
-    for (j = 0; j <= i; j++) {
+  for (i = 0; i < n; i++) 
+  {
+    for (j = 0; j <= i; j++) 
+    {
       printf("%5d ", T[i][j]);
     }
     printf("\n");
   }
-
-  return;
 }
 
 
-int main() {
+int main()
+{
   int n,i,j,**T;
 
   printf("Quantos numeros do coiso de pascal? ");
   scanf("%d", &n);
 
   T = (int **) malloc(n*sizeof(int *));
-  if (T == NULL) {
+  if (T == NULL) 
+  {
     printf("Erow\n");
     return(-1);
   }
   
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n; i++) 
+  {
     T[i] = (int *) malloc((i+1)*sizeof(int));
     
     if (T[i] == NULL) {
@@ -60,7 +66,8 @@ int main() {
     }
   }
 
-  if (n <= 0) {
+  if (n <= 0) 
+  {
     return(1);
   }
 
