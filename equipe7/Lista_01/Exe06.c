517bc3baa7685ba1d931812eb30f609e27669b7b
Paulo90 <psj.jennings@hotmail.com>
2016-08-18 14:34:13 -0400

    Create Exe06.c

---
+++ equipe7/Lista_01/Exe06.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+ int main()
+ {
+
+    int linha, espacos, num_linhas, r, elemento;
+// r = posicao do elemento em sua linha
+
+    printf("Insira o numero de linhas do triangulo: ");
+    scanf("%d", &num_linhas);
+
+// logica de preenchimento do triangulo    
+    for(linha=0; linha<num_linhas; linha++)
+    {
+        for(espacos=1; espacos<num_linhas - linha; espacos++)
+        {
+            printf(" ");
+        }
+        for(r = 0; r <= linha; r++)
+        {
+            if(linha == 0 || r == 0)
+            {
+                elemento = 1;
+                printf("%d ", elemento);
+            }
+            else
+            {
+                elemento = elemento * (linha - r + 1)/r;
+                printf("%d ", elemento);
+            }
+        }
+    printf("\n");
+    }
+return 0;
+}
