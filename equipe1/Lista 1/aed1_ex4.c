4a1d08ca512e51e0c8a2169b61ca1d6d651267fc
aluno <you@example.com>
2016-08-17 21:15:42 -0400

    padronizando

--- equipe1/Lista 1/aed1_ex4.c
+++ equipe1/Lista 1/aed1_ex4.c
@@ -1,37 +1,43 @@
-/* equipe:
-	rodrigo oliveira
-	fernando calderaro
-	thailana neves
-	diogo abreu
-	leticia mota		*/
+/* equipe 01:
+	Diogo Abreu
+	Fernando Calderaro
+	Leticia Mota
+	Rodrigo Oliveira
+	Thailana Neves	
 
-//4. Dados dois vetores x e y, ambos com n elementos, determinar o produto escalar desses vetores.
+	exercicio 04
+
+	Dados dois vetores x e y, ambos com n elementos, determinar o produto escalar desses vetores.
+*/
 
 #include <stdio.h>
 
 int main () {
-    int i, n;
-    float produtoe, vetx[10], vety[10];
+	int i, n;
+	float produtoe, vetx[10], vety[10];
 
-    produtoe = 0;
+	produtoe = 0;
 
-    printf("Digite o tamanho dos vetores: ");
-    scanf("%d", &n);
+	printf("Digite o tamanho dos vetores: ");
+	scanf("%d", &n);
 
-    printf("Digite os componentes do vetor x: ");
-    for (i=0; i<n; i++) {
-        scanf("%f", &vetx[i]);
-    }
+	printf("Digite os componentes do vetor x: ");
+	for (i=0; i<n; i++)
+	{
+		scanf("%f", &vetx[i]);
+	}
 
-    printf("Digite os componentes do vetor y: ");
-    for (i=0; i<n; i++) {
-        scanf("%f", &vety[i]);
-    }
+	printf("Digite os componentes do vetor y: ");
+	for (i=0; i<n; i++)
+	{
+		scanf("%f", &vety[i]);
+	}
 
-    for (i=0; i<n; i++) {
-        produtoe = produtoe + vetx[i] * vety[i]; //cálculo do produto escalar
-    }
+	for (i=0; i<n; i++)
+	{
+		produtoe = produtoe + vetx[i] * vety[i]; //cálculo do produto escalar
+	}
 
-    printf("O produto escalar desses vetores é %.2f", produtoe);
-    return 0;
+	printf("O produto escalar desses vetores é %.2f", produtoe);
+	return 0;
 }
