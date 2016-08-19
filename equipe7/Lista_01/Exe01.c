f616a4569ecf323c7e341ad22be9c7ae687519ef
fabianabezerra <fabisouza.bezerra@gmail.com>
2016-08-18 20:10:12 -0400

    Update Exe01.c

--- equipe7/Lista_01/Exe01.c
+++ equipe7/Lista_01/Exe01.c
@@ -25,7 +25,8 @@ int main()
 {
     float **matrx = (float**) malloc (sizeof(float*) * MAX);
     int i,j;
-    for(i=0;i<MAX;i++){
+    for(i=0;i<MAX;i++)
+    {
         matrx[i] = (float*) malloc (sizeof(float)*MAX);
         for(j=0;j<MAX;j++)
             matrx[i][j]=0.0;
