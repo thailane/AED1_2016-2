a7c124bf9ca79c7fad22957d225c2112f6a93cf2
fabianabezerra <fabisouza.bezerra@gmail.com>
2016-08-18 20:12:30 -0400

    Update Exe06.c

--- equipe7/Lista_01/Exe06.c
+++ equipe7/Lista_01/Exe06.c
@@ -20,16 +20,21 @@
     scanf("%d", &num_linhas);
 
 // logica de preenchimento do triangulo    
-    for(linha=0; linha<num_linhas; linha++){
-        for(espacos=1; espacos<num_linhas - linha; espacos++){
+    for(linha=0; linha<num_linhas; linha++)
+    {
+        for(espacos=1; espacos<num_linhas - linha; espacos++)
+        {
             printf(" ");
         }
-        for(r = 0; r <= linha; r++){
-            if(r == 0 || r == linha){
+        for(r = 0; r <= linha; r++)
+        {
+            if(r == 0 || r == linha)
+            {
                 elemento = 1;
                 printf("%d ", elemento);
             }
-            else{
+            else
+            {
                 elemento = elemento * (linha - r + 1)/r;
                 printf("%d ", elemento);
             }
